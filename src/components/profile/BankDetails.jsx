a30b9ed570cc50f8c74b40696eca6eb7cda37a8b
zenmeraki7 <zenmerakihelp@gmail.com>
2025-01-10 16:14:32 +0530

    added api integration in bank details edit tab

--- src/components/profile/BankDetails.jsx
+++ src/components/profile/BankDetails.jsx
@@ -6,36 +6,101 @@ import DriveFolderUploadIcon from "@mui/icons-material/DriveFolderUpload";
 import CustomInput from "../SharedComponents/CustomInput";
 import DeleteIcon from "@mui/icons-material/Delete";
 import CustomButton from "../SharedComponents/CustomButton";
+import CustomSelect from "../SharedComponents/CustomSelect";
+import { BASE_URL } from "../../utils/baseUrl";
+import axios from "axios";
+import * as Yup from "yup";
+import toast from "react-hot-toast";
+
+const bankvalidationSchema = Yup.object({
+  accountHolderName: Yup.string().required("Account Holder Name is required"),
+  accountNumber: Yup.string()
+    .required("Account Number is required")
+    .matches(/^\d{9,18}$/, "Invalid Account Number format"),
+  ifscCode: Yup.string()
+    .required("IFSC Code is required")
+    .matches(/^[A-Za-z]{4}[0][A-Za-z0-9]{6}$/, "Invalid IFSC Code format"),
+  bankName: Yup.string().required("Bank Name is required"),
+});
 
 function BankDetails({ bankDetails }) {
-  const [isModalOpen, setIsModalOpen] = useState(false);
   const [editedBankDetails, setEditedBankDetails] = useState(bankDetails);
-
+  const [editable, setEditable] = useState(false);
+  const [validationError, setValidationError] = useState({});
+  const [banks, setBanks] = useState([]);
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState(bankDetails?.documentUrl || "");
+  // Fetch the banks on component mount
   useEffect(() => {
-    setEditedBankDetails(bankDetails); // Update state when bankDetails prop changes
-  }, [bankDetails]);
+    const fetchBanks = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/api/banks`);
+        setBanks(response.data); // Set the fetched banks to state
+      } catch (error) {
+        console.error("Error fetching banks:", error);
+      }
+    };
 
-  const handleInputChange = (e, field) => {
-    setEditedBankDetails({ ...editedBankDetails, [field]: e.target.value });
+    fetchBanks();
+  }, []);
+  console.log(validationError);
+  const handleSubmit = async () => {
+    try {
+      await bankvalidationSchema.validate(editedBankDetails, {
+        abortEarly: false,
+      });
+      const formData = new FormData();
+      formData.append("accountHolderName", editedBankDetails.accountHolderName);
+      formData.append("accountNumber", editedBankDetails.accountNumber);
+      formData.append("ifscCode", editedBankDetails.ifscCode);
+      formData.append("bankName", editedBankDetails.bankName);
+      if (image && image instanceof File) {
+        formData.append("image", image);
+      }
+      const token = localStorage.getItem("token");
+      toast.loading("Updating bank details...");
+      const response = await axios.put(
+        `${BASE_URL}/api/vendor/update-bank-details`,
+        formData, // API endpoint
+        {
+          headers: {
+            authorization: `Bearer ${token}`, // Include token for authentication
+            "Content-Type": "multipart-form/data",
+          },
+        }
+      );
+      setValidationError({});
+      console.log("bank details updated successfully:", response.data);
+      toast.dismiss();
+      toast.success("Bank details updated successfully");
+      setEditable(false);
+      setImage(null);
+    } catch (err) {
+      toast.dismiss();
+      if (err.name === "ValidationError") {
+        const errorMessages = err.inner.reduce((acc, curr) => {
+          acc[curr.path] = curr.message;
+          return acc;
+        }, {});
+        setValidationError(errorMessages);
+      } else {
+        toast.error(err.response?.data?.message || "An error occurred.");
+      }
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { value, name } = e.target;
+    setEditedBankDetails({ ...editedBankDetails, [name]: value });
   };
 
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        setEditedBankDetails((prev) => ({
-          ...prev,
-          documentUrl: reader.result, // Save base64 image
-        }));
-      };
-      reader.readAsDataURL(file);
+      const logoURL = URL.createObjectURL(file);
+      setImageUrl(logoURL);
+      setImage(file);
     }
-  };
-
-  const handleSaveClick = () => {
-    console.log("Updated bank details:", editedBankDetails);
-    setIsModalOpen(false); // Close the modal
   };
 
   return (
@@ -75,26 +140,39 @@ function BankDetails({ bankDetails }) {
           <Stack direction={"row"} spacing={2}>
             <Stack spacing={1.5} flex={1}>
               <CustomInput
+                error={validationError.accountHolderName}
                 label="Account Holder Name"
                 value={editedBankDetails.accountHolderName}
-                onChange={(e) => handleInputChange(e, "accountHolderName")}
+                name={"accountHolderName"}
+                readOnly={!editable}
+                onChange={handleInputChange}
               />
               <CustomInput
+                error={validationError.accountNumber}
                 label="Account Number"
                 value={editedBankDetails.accountNumber}
-                onChange={(e) => handleInputChange(e, "accountNumber")}
+                name={"accountNumber"}
+                readOnly={!editable}
+                onChange={handleInputChange}
               />
             </Stack>
-            <Stack spacing={1.5} flex={1}>
+            <Stack spacing={3} flex={1}>
               <CustomInput
+                error={validationError.ifscCode}
                 label="IFSC Code"
                 value={editedBankDetails.ifscCode}
-                onChange={(e) => handleInputChange(e, "ifscCode")}
+                name={"ifscCode"}
+                readOnly={!editable}
+                onChange={handleInputChange}
               />
-              <CustomInput
-                label="Bank Name"
-                value={editedBankDetails.bankName.name}
-                onChange={(e) => handleInputChange(e, "bankName")}
+              <CustomSelect
+                error={validationError.bankName}
+                label={"Bank Name"}
+                value={editedBankDetails.bankName}
+                name={"bankName"}
+                readOnly={!editable}
+                onChange={handleInputChange}
+                MenuItems={banks}
               />
             </Stack>
           </Stack>
@@ -110,7 +188,7 @@ function BankDetails({ bankDetails }) {
         >
           <img
             src={
-              editedBankDetails.documentUrl ||
+              imageUrl ||
               "https://cdn.pixabay.com/photo/2016/03/31/14/48/sheet-1292828_960_720.png"
             }
             alt="Bank Document"
@@ -122,16 +200,14 @@ function BankDetails({ bankDetails }) {
               border: "1px solid #ccc",
             }}
           />
-          <label htmlFor="upload-button">
-            <input
-              id="upload-button"
-              type="file"
-              accept="image/*"
-              style={{ display: "none" }}
-              onChange={handleFileChange}
-            />
-            <>
-              <Stack direction={"row"} spacing={1}>
+          <Box sx={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+            <label>
+              <input
+                type="file"
+                style={{ display: "none" }}
+                onChange={handleFileChange}
+              />
+              {editable && (
                 <DriveFolderUploadIcon
                   sx={{
                     fontSize: "36px",
@@ -142,7 +218,12 @@ function BankDetails({ bankDetails }) {
                     borderRadius: "50%",
                   }}
                 />
+              )}
+            </label>
+            {image && (
+              <>
                 <SaveIcon
+                  // onClick={uploadCompanyIcon}
                   sx={{
                     fontSize: "36px",
                     color: "#fff",
@@ -153,6 +234,10 @@ function BankDetails({ bankDetails }) {
                   }}
                 />
                 <DeleteIcon
+                  onClick={() => {
+                    setImageUrl(bankDetails.documentUrl || "");
+                    setImage(null);
+                  }}
                   sx={{
                     fontSize: "36px",
                     color: "#fff",
@@ -162,25 +247,49 @@ function BankDetails({ bankDetails }) {
                     borderRadius: "50%",
                   }}
                 />
-              </Stack>
-            </>
-          </label>
+              </>
+            )}
+          </Box>
         </Box>
       </Stack>
 
-      {/* Edit & Save Actions */}
-      <Stack
-        direction="row"
-        justifyContent="flex-end"
-        alignItems="center"
-        marginTop="2rem"
+      <Box
+        sx={{
+          textAlign: "right",
+          marginTop: "20px",
+          display: "flex",
+          gap: "20px",
+          justifyContent: "flex-end",
+        }}
       >
-        <CustomButton
-          onClick={() => setIsModalOpen(true)}
-          label="Edit Details"
-          icon={<ModeEditIcon sx={{ marginRight: "8px" }} />}
-        />
-      </Stack>
+        {!editable && (
+          <CustomButton
+            onClick={() => setEditable(true)}
+            label="Edit Details"
+            icon={<ModeEditIcon sx={{ marginRight: "8px" }} />}
+          />
+        )}
+        {editable && (
+          <Stack direction={"row"} spacing={2}>
+            <CustomButton
+              onClick={handleSubmit}
+              label="Save"
+              icon={<SaveIcon sx={{ marginRight: "8px" }} />}
+            />
+            <CustomButton
+              onClick={() => {
+                setEditedBankDetails(bankDetails);
+                setEditable(false);
+                setValidationError({});
+                setImageUrl(bankDetails.documentUrl || "");
+                setImage(null);
+              }}
+              label="Cancel"
+              variant="outlined"
+            />
+          </Stack>
+        )}
+      </Box>
     </Box>
   );
 }
